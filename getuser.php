ae5711f1a49be36cb4210a9d463dd7bfd9165c3f
Robert Panduru <36996206+Swagstar21@users.noreply.github.com>
2019-04-22 22:45:03 +0300

    Add files via upload

--- getuser.php
+++ getuser.php
@@ -1,51 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
-</head>
-<body>
-
 <?php
-$q = intval($_GET['q']);
 
 $con = mysqli_connect('localhost','barosan','barosan','my_db');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"user");
-$sql="SELECT * FROM user WHERE id = '".$q."'";
-$result = mysqli_query($con,$sql);
+mysqli_select_db($con,"ofertanti");
+$sql="SELECT Username, Password FROM ofertanti where Username = ? and Password = ?";
 
-echo "<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Age</th>
-<th>Hometown</th>
-<th>Job</th>
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['FirstName'] . "</td>";
-    echo "<td>" . $row['LastName'] . "</td>";
-    echo "<td>" . $row['Age'] . "</td>";
-    echo "<td>" . $row['Hometown'] . "</td>";
-    echo "<td>" . $row['Job'] . "</td>";
-    echo "</tr>";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("ss", $_GET['uname'], $_GET['psw']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($name, $pass);
+$stmt->fetch();
+$stmt->close();
+
+if (!isset($name) || trim($name) === '') {
+    echo "Invalid username/password";
 }
-echo "</table>";
+else {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Username</th>";
+    echo "<td>" . $name . "</td>";
+    echo "<th>Password</th>";
+    echo "<td>" . $pass . "</td>";
+    echo "</tr>";
+    echo "</table>";
+    echo "Logged in succesfully";
+}
+
 mysqli_close($con);
 ?>
