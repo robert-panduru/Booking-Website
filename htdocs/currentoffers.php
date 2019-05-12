b90627abf93a2ce69978b626ddd00a158306e894
Ursu Alexandru-Cristian <36843588+I0MANIP@users.noreply.github.com>
2019-05-12 19:37:27 +0300

    Add files via upload

--- htdocs/currentoffers.php
+++ htdocs/currentoffers.php
@@ -54,24 +54,54 @@ echo "<!DOCTYPE html>
     </style>
     </div>
 </head>
-<body>
+<body background=\"http://bramptonist.com/wp-content/uploads/2017/03/sports-field.jpg\">
+
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>
 ";
+echo "<a href=\"#\" class=\"btn btn-primary\" style=\"position:absolute; TOP:10%; LEFT:10%;\">Add offer</a>";
 if ($result->num_rows > 0) {
+    echo "<table class=\"table table-dark\" style=\"position:absolute; TOP:20%; LEFT:10%; WIDTH:80%\">
+          <thead>
+            <tr>
+              <th scope=\"col\">#</th>
+              <th scope=\"col\">Nume anunt</th>
+              <th scope=\"col\">Locatie</th>
+              <th scope=\"col\">Pret</th>
+              <th scope=\"col\">Data inceput</th>
+              <th scope=\"col\">Data final</th>
+            </tr>
+          </thead>
+          <tbody>";
     // output data of each row
+    $register_number = 0;
     while($row = $result->fetch_assoc()) {
-        echo "<ul class=\"list-group list-group-flush\">
+        echo "<tr>
+              <th scope=\"row\">$register_number</th>
+              <td>" . $row["Titlu"] . "</td>
+              <td>" . $row["Adresa"] . "</td>
+              <td>" . $row["Pret"] . "</td>
+              <td>" . $row["Inceput"] . "</td>
+              <td>" . $row["Final"] . "</td>
+              </tr>";
+        $register_number++;
+
+/*        echo "<ul class=\"list-group list-group-flush\">
             <li class=\"list-group-item\" style=\"margin-top:10px;\">ID anunt: " . $row["ID_anunt"] . "</li>
             <li class=\"list-group-item\" style=\"margin-top:10px;\">Titlu:  " . $row["Titlu"] . "</li>
             <li class=\"list-group-item\" style=\"margin-top:10px;\">Adresa:  " . $row["Adresa"] . "</li>
             <li class=\"list-group-item\" style=\"margin-top:10px;\">Pret:  " . $row["Pret"] . "</li>
             <li class=\"list-group-item\" style=\"margin-top:10px;\">Incepe la data de:  " . $row["Inceput"] . "</li>
             <li class=\"list-group-item\" style=\"margin-top:10px;\">Se termina la data de:  " . $row["Final"] . "</li>
-    </ul><br>";
+    </ul><br>";*/
     }
+    echo "</tbody>
+        </table>";
 }
 
-echo "</body>
-    </html>";
+echo "    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>
+    </body>
+</html>";
 
 mysqli_close($con);
 ?>
