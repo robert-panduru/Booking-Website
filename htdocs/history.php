444b5e5ece9bdfe9946900e5491de426ac90d716
Ursu Alexandru-Cristian <36843588+I0MANIP@users.noreply.github.com>
2019-05-18 22:33:52 +0300

    Add files via upload

--- htdocs/history.php
+++ htdocs/history.php
@@ -13,14 +13,78 @@ AND B.ID_ofertant = " .$id;
 
 $result = mysqli_query($con, $sql);
 
+
+echo "<!DOCTYPE html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=yes\">
+    <!-- Bootstrap CSS -->
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">
+    <style type=\"text/css\">
+        /*// Small devices (landscape phones, 576px and up)*/
+        @media (min-width: 576px) { ... }
+        /*// Medium devices (tablets, 768px and up)*/
+        @media (min-width: 768px) { ... }
+        /*// Large devices (desktops, 992px and up)*/
+        @media (min-width: 992px) { ... }
+        /*// Extra large devices (large desktops, 1200px and up)*/
+        @media (min-width: 1200px) { ... }
+        /*// No media query necessary for xs breakpoint as it's effectively `@media (min-width: 0) { ... }`*/
+        @include media-breakpoint-up(sm) { ... }
+        @include media-breakpoint-up(md) { ... }
+        @include media-breakpoint-up(lg) { ... }
+        @include media-breakpoint-up(xl) { ... }
+        /*// Example: Hide starting at `min-width: 0`, and then show at the `sm` breakpoint*/
+        .custom-class {
+          display: none;
+        }
+        @include media-breakpoint-up(sm) {
+          .custom-class {
+            display: block;
+          }
+        .info_panel {
+            position: absolute;
+            width: 40%;
+            left: 10%;
+            top: 5%;
+        }
+    </style>
+    </div>
+</head>
+<body background=\"http://bramptonist.com/wp-content/uploads/2017/03/sports-field.jpg\">
+    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>
+";
+
+$review_number = 0;
 if ($result->num_rows > 0) {
+	echo "<table class=\"table table-hover\" style=\"position:absolute; TOP:20%; LEFT:10%; WIDTH:80%\">
+          <thead class=\"thead-dark\">
+            <tr>
+              <th scope=\"col\">#</th>
+              <th scope=\"col\">Titlu anunt</th>
+              <th scope=\"col\">Nume</th>
+              <th scope=\"col\">Prenume</th>
+              <th scope=\"col\">Recenzie</th>
+            </tr>
+          </thead>
+          <tbody>";
 	while($row = $result->fetch_assoc()) {
-		echo $row["Titlu"] . " " . $row["Nume"] . " " . $row["Prenume"] . " " . $row["Comm"] . "<br>";
+		        echo "<tr class=\"table-primary\">
+		              <th scope=\"row\">$review_number</th>
+		              <td>" . $row["Titlu"] . "</td>
+		              <td>" . $row["Nume"] . "</td>
+		              <td>" . $row["Prenume"] . "</td>
+		              <td>" . $row["Comm"] . "</td>
+		              </tr>";
+        $review_number++;
 	}
+	echo "</tbody>
+          </table>";
 
 }
 else {
-	echo "Nu e nimic de listat";
+	echo "Nu a fost listat nimic";
 }
 
 mysqli_close($con);
